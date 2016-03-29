44f51a8e619c14bfa306cd9a762a903c1ad95d66
Wojciech Langiewicz <wlangiewicz@gmail.com>
2016-03-29 17:18:58 +0200

    wip adding tests

--- test/models/v1/CardsSpec.scala
+++ test/models/v1/CardsSpec.scala
@@ -15,8 +15,43 @@ class CardsSpec extends FlatSpec with Matchers {
     val aceOfSpaces = Card(Ace, Spades)
     val tenOfSpaces = Card(Ten, Spades)
     val twoOfHearts = Card(Two, Hearts)
+    val aceOfDiamonds = Card(Ace, Diamonds)
 
-    (aceOfSpaces > tenOfSpaces) shouldBe true
-    (twoOfHearts > tenOfSpaces) shouldBe false
+    aceOfSpaces > tenOfSpaces shouldBe true
+    twoOfHearts > tenOfSpaces shouldBe false
+
+    aceOfSpaces > aceOfDiamonds shouldBe false
+    aceOfDiamonds > aceOfSpaces shouldBe false
+  }
+
+  it should "match cards in pairs" in {
+    val aceOfSpaces = Card(Ace, Spades)
+    val tenOfSpaces = Card(Ten, Spades)
+    val twoOfHearts = Card(Two, Hearts)
+    val aceOfDiamonds = Card(Ace, Diamonds)
+
+    aceOfDiamonds.isPairWith(aceOfSpaces) shouldBe true
+    aceOfSpaces.isPairWith(aceOfDiamonds) shouldBe true
+
+    twoOfHearts.isPairWith(tenOfSpaces) shouldBe false
+  }
+
+  it should "shuffled deck should contain the same cards as not shuffled" in {
+    val deck = Deck.fullDeck
+    val shuffledDeck = deck.shuffle
+
+    shuffledDeck.cards should contain theSameElementsAs deck.cards
+  }
+
+  it should "match cards in suits" in {
+    val aceOfSpaces = Card(Ace, Spades)
+    val twoOfSpaces = Card(Two, Spades)
+    val aceOfDiamonds = Card(Ace, Diamonds)
+
+    aceOfDiamonds.isSameSuit(aceOfSpaces) shouldBe false
+    aceOfSpaces.isSameSuit(aceOfDiamonds) shouldBe false
+
+    aceOfSpaces.isSameSuit(twoOfSpaces) shouldBe true
+    twoOfSpaces.isSameSuit(aceOfSpaces) shouldBe true
   }
 }
