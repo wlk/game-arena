1fec818394a66230fec764837d2c51ef7b0daa39
Wojciech Langiewicz <wlangiewicz@gmail.com>
2016-03-30 19:55:47 +0200

    refactored areAllConsecutive

--- app/models/HandExtractor.scala
+++ app/models/HandExtractor.scala
@@ -1,14 +1,21 @@
 package models
 
 import models.v1.Card
-import models.v1.Rank._
 
 /*
   Responsible for extracting information about what hands can be arranged from a list of cards.
  */
 class HandExtractor {
 
-  private def areAllConsecutive(cards: List[Card]): Boolean = !cards.map(card => ranks.indexOf(card.rank)).sorted.reverse.sliding(2).map(e => e.head - e(1)).exists(e => e != 1)
+  private def areAllConsecutive(cards: List[Card]): Boolean = {
+    val cardsMappedToRankIndexes = cards.map(card => models.v1.Rank.ranks.indexOf(card.rank)).sorted.reverse
+    val consecutiveCardPairs = cardsMappedToRankIndexes.sliding(2)
+
+    consecutiveCardPairs.forall {
+      case first :: second :: _ => first - second == 1
+      case _                    => false
+    }
+  }
 
   private def areAllSameSuit(cards: List[Card]): Boolean = cards.map(_.suit).distinct.size == 1
 
